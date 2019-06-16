5a68c50932d046889a5566a480dec8c8d0f90be6
fndiaz <fndiaz02@gmail.com>
2019-06-16 15:11:06 -0300

    v0.04

--- devops/jenkins-generator/deploy_jobs.groovy
+++ devops/jenkins-generator/deploy_jobs.groovy
@@ -1,7 +1,7 @@
 def slurper = new ConfigSlurper()
 // fix classloader problem using ConfigSlurper in job dsl
 slurper.classLoader = this.class.classLoader
-def config = slurper.parse(readFileFromWorkspace('devops/jenkins-generator/build_jobs.dsl'))
+def config = slurper.parse(readFileFromWorkspace('devops/jenkins-generator/variables.dsl'))
 
 config.app_job.each { name, data ->
   println "generating deploy $name"
@@ -41,7 +41,7 @@ def createJob(app, data){
 
 		wrappers {
             credentialsBinding {
-            usernamePassword('DOCKERUSER', 'DOCKERPASS', 'dockerhub')
+            usernamePassword('DOCKERUSER', 'DOCKERPASS', data.credentials)
         }
     	}
 
