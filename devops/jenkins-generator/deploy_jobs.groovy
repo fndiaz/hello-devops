116a4a0324803e7b7bc24f44d95c2a120f4cac58
fndiaz <fndiaz02@gmail.com>
2019-06-16 14:51:46 -0300

    v0.04

---
+++ devops/jenkins-generator/deploy_jobs.groovy
@@ -0,0 +1,110 @@
+def slurper = new ConfigSlurper()
+// fix classloader problem using ConfigSlurper in job dsl
+slurper.classLoader = this.class.classLoader
+def config = slurper.parse(readFileFromWorkspace('devops/jenkins-generator/build_jobs.dsl'))
+
+config.app_job.each { name, data ->
+  println "generating deploy $name"
+  //println name
+  //println data
+  createJob(name, data)
+}
+
+config.list_views.each { name, data ->
+  println "generating deploy $name"
+  //println name
+  //println data
+  createView(name, data)
+}
+
+
+
+
+def createJob(app, data){
+
+	job("deploy-${app}") {
+		scm {
+	    	git {
+				remote {
+	            	url(data.url)
+	            }
+	        	branch(data.branch)
+	    	}
+		}
+
+		logRotator {
+			daysToKeep(-1)
+			numToKeep(data.rotate_builds)
+			artifactDaysToKeep(-1)
+			artifactNumToKeep(-1)
+		}
+
+		wrappers {
+            credentialsBinding {
+            usernamePassword('DOCKERUSER', 'DOCKERPASS', 'dockerhub')
+        }
+    	}
+
+	    steps {
+			shell(getShell(app, data))
+	    }
+
+        try {
+		     publishers {
+        		buildPipelineTrigger("deploy-${app}")
+    		}
+        }
+        catch (MissingPropertyExceptionmpe) {
+        	println 'pos build nao configurado'
+        }
+
+
+        try {
+        	triggers {
+            	cron(data."${enviroment}".cron)
+            }
+        }
+        catch (MissingPropertyExceptionmpe) {
+        	println 'cron nao configurado'
+        }
+
+	}
+}
+
+
+private String getShell(app, data) {
+
+    String var_shell
+    var_shell="""
+	cd devops/kubernetes
+	kubectl set image deployment/${app} ${app}=${data.user_dockerhub}/${app}:\$GIT_COMMIT"""
+
+ 	return var_shell
+}
+
+
+def createView(app, data) {
+
+	listView("${app}") {
+	    description("${data.description}")
+	    filterBuildQueue()
+	    filterExecutors()
+	    jobs {
+	        //name('release-projectA')
+	        regex(/${data.regex}/)
+	    }
+	    columns {
+	        status()
+	        weather()
+	        name()
+	        lastSuccess()
+	        lastFailure()
+	        lastDuration()
+	        buildButton()
+	    }
+	}
+
+}
+
+
+
