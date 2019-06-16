096c26c8d216a52a98c30e36de892a96d7b8a5bc
fndiaz <fndiaz02@gmail.com>
2019-06-16 02:20:34 -0300

    v0.02

--- devops/jenkins-generator/create_envs.groovy
+++ devops/jenkins-generator/create_envs.groovy
@@ -52,6 +52,7 @@ kubectl create -f service-mysql.yml
 kubectl create -f service-rabbit.yml
 kubectl create -f service-rabbit-web.yml
 POD=\$(kubectl get pod -l app=mysql -o jsonpath="{.items[0].metadata.name}")
+sleep 20
 kubectl exec -ti \$POD sh /usr/local/mysql-init.sh
 kubectl create -f deploy-hello-node.yml
 ELB=\$(kubectl get services service-hello-python -o jsonpath="{.status.loadBalancer.ingress[0].hostname}")
