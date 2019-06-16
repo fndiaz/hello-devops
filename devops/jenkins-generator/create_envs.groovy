cb71e95a0cc90f2e9c7a376791722a551bfca55a
fndiaz <fndiaz02@gmail.com>
2019-06-16 01:55:07 -0300

    v0.01

--- devops/jenkins-generator/create_envs.groovy
+++ devops/jenkins-generator/create_envs.groovy
@@ -42,17 +42,26 @@ private String getShell() {
     String var_shell
     var_shell="""
 cd devops/kubernetes
+kubectl get svc service-hello-python
 kubectl create -f configmap-script-mysql.yml
 kubectl create -f pv-mysql.yml
 kubectl create -f deploy-mysql.yml
 kubectl create -f deploy-rabbit.yml
 kubectl create -f deploy-hello-python.yml
-kubectl create -f deploy-hello-node.yml
 kubectl create -f service-mysql.yml
 kubectl create -f service-rabbit.yml
 kubectl create -f service-rabbit-web.yml
 kubectl create -f service-hello-python.yml
-kubectl get svc service-hello-python"""
+POD=$(kubectl get pod -l app=mysql -o jsonpath="{.items[0].metadata.name}")
+kubectl exec -ti $POD sh /usr/local/mysql-init.sh
+kubectl create -f deploy-hello-node.yml
+
+ELB=$(kubectl get services service-hello-python -o jsonpath="{.status.loadBalancer.ingress[0].hostname}")
+
+echo "URL: http://$ELB"
+
+echo 
+"""
 
   return var_shell
 }
